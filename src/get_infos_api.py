3285a315dafbe20f165ed66503bee0e610eeecd7
Martin <martin.d-herouville@epitech.eu>
2022-03-28 17:38:39 +0200

    add request functions

---
+++ src/get_infos_api.py
@@ -0,0 +1,7 @@
+import requests
+
+def r_get_json_request(link:str):
+    return (requests.get(link).json())
+
+def r_get_last_project(link:str):
+    r = r_get_json_request("https://intra.epitech.eu/planning/load?format=json")[-1]
