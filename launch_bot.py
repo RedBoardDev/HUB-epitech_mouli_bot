3cef4bd8f08f90b7e2f276ff3bef3fad72dfabc2
Martin <martin.d-herouville@epitech.eu>
2022-03-28 17:05:24 +0200

    respond to messages

--- launch_bot.py
+++ launch_bot.py
@@ -2,8 +2,17 @@ import discord
 from dotenv import dotenv_values
 
 client = discord.Client()
+prefix = '$'
 secrets = dotenv_values(".env")
 token_discord = secrets["TOKEN_DISCORD"]
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    msg = message.content
+    if msg.startswith(prefix + 'oui'):
+        await message.channel.send("non")
 
 @client.event
 async def on_ready():
